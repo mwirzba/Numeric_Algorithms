df2b226ec040c7edf3716b9e79a2c38006c256d3
mwirzba <mateuszwir23@gmail.com>
2019-10-10 02:53:42 +0200

    Comming First Algorithm

---
+++ SummationOfPowerSeries/SummationOfPowerSeries/Algorithm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SummationOfPowerSeries
+{
+    class Algorithm
+    {
+        public Algorithm()
+        {
+           ResultsList =  new List<double>();
+           ResultsList2 = new List<double>();
+        }
+        public List<double> ResultsList { get; private set; }
+        public List<double> ResultsList2 { get; private set; }
+        public void CountUsingFormula(double x,int n)
+        {
+
+            double tmpX = x-1;
+            double tmp = tmpX;
+            ResultsList.Clear();
+            ResultsList.Add(tmpX);
+            for (int i = 2; i <= n; i++)
+            {
+                tmp = tmp * tmpX * (-1);
+                ResultsList.Add(tmp/i);
+            }
+        }
+
+        public void CountUsingPreviousResult(double x, int n)
+        {
+            ResultsList2.Clear();
+            double tmpX = x - 1;
+            ResultsList2.Add(tmpX);
+            for (int i = 1; i < n; i++)
+            {
+                var tmp = tmpX * ((-1) * i * (x - 1)) / (i + 1);
+                tmpX = tmp;
+                ResultsList2.Add(tmp);
+            }
+
+        }
+
+        public double SumFromBeginning(List<double> resultsList)
+        {
+            double result=0;
+            for (int i = 0; i<resultsList.Count; i++)
+            {
+
+                result += resultsList[i];
+            }
+
+            return result;
+        }
+
+        public double SumFromEnd(List<double> resultsList)
+        {
+            double result = 0;
+            for (int i = resultsList.Count-1; i >= 0; i--)
+            {
+
+                result += resultsList[i];
+            }
+
+            return result;
+        }
+
+        public List<double> GetArgumentsList(double amount)
+        {
+            var rtnList = new List<double>();
+            var random = new Random();
+            for (int i = 0; i < amount; i++)
+            {
+                double randomNumber = random.NextDouble() * 2;
+                rtnList.Add(randomNumber);
+            }
+
+            return rtnList;
+
+        }
+    }
+}
---
+++ SummationOfPowerSeries/SummationOfPowerSeries/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace SummationOfPowerSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Algorithm alg = new Algorithm();
+            double numberOfArgs = 2000000;
+            List<double> argumentsList = alg.GetArgumentsList(numberOfArgs);
+            List<double> formulaResultsFromBegin =  new List<double>();
+            List<double> formulaResultsFromEnd = new List<double>();
+
+            List<double> countUsingPreviousWordResultsFromBegin = new List<double>();
+            List<double> countUsingPreviousWordResultsFromEnd = new List<double>();
+
+            foreach (var arg in argumentsList)
+            {
+                alg.CountUsingFormula(arg,30);
+                alg.CountUsingPreviousResult(arg,30);
+
+                formulaResultsFromBegin.Add(alg.SumFromBeginning(resultsList: alg.ResultsList));
+                formulaResultsFromEnd.Add(alg.SumFromEnd(resultsList: alg.ResultsList));
+
+                countUsingPreviousWordResultsFromBegin.Add(alg.SumFromBeginning(resultsList: alg.ResultsList2));
+                countUsingPreviousWordResultsFromEnd.Add(alg.SumFromEnd(resultsList: alg.ResultsList2));
+            }
+
+
+            Console.WriteLine(Program.Compare(formulaResultsFromBegin, formulaResultsFromEnd,argumentsList)/numberOfArgs);
+            Console.WriteLine(Program.Compare(countUsingPreviousWordResultsFromBegin, countUsingPreviousWordResultsFromEnd, argumentsList)/numberOfArgs);
+
+
+            Console.WriteLine();
+
+            Console.WriteLine(Program.Compare(formulaResultsFromBegin, countUsingPreviousWordResultsFromBegin, argumentsList) / numberOfArgs);
+            Console.WriteLine(Program.Compare(formulaResultsFromEnd, countUsingPreviousWordResultsFromEnd, argumentsList) / numberOfArgs);
+
+       
+
+            /* alg.CountUsingFormula(1.55,8);
+             double rtn = alg.SumFromBeginning(resultsList: alg.ResultsList);
+             double rtn2 = alg.SumFromEnd(resultsList: alg.ResultsList);
+             alg.CountUsingPreviousResult(1.55,8);
+
+
+             double rtn1 = alg.SumFromBeginning(resultsList: alg.ResultsList2);
+             double rtn12 = alg.SumFromEnd(resultsList: alg.ResultsList2);
+
+             Console.WriteLine("1.1: "+rtn);
+             Console.WriteLine("2.1: "+ rtn2);
+             Console.WriteLine("Log: "+ Math.Log(1.55));
+
+
+             Console.WriteLine("1.2: " + rtn1);
+             Console.WriteLine("2.2: " + rtn12);
+             Console.WriteLine("Log: " +Math.Log(1.55));
+             */
+
+            Console.ReadKey();
+
+        }
+
+        static int Compare(List<double> formulaResults, List<double> countUsingPreviousWordResults, List<double> args)
+        {
+            int numberOfMoreAccurateResults=0;
+            for (int i = 0; i < formulaResults.Count; i++)
+            {
+                double tmp = Math.Log(args[i]);
+                if ( tmp - countUsingPreviousWordResults[i] < tmp - formulaResults[i])
+                {
+                    numberOfMoreAccurateResults++;
+                }
+            }
+
+            return numberOfMoreAccurateResults;
+
+        }
+
+        /*static int CheckIfAlgorithmUsingPreviousWordIsMoreAccurate(List<double> resultsFromBeginning, List<double> resultsFromEnd, List<double> args)
+        {
+            int numberOfMoreAccurateResults = 0;
+            for (int i = 0; i < resultsFromBeginning.Count; i++)
+            {
+                double tmp = Math.Log(args[i]);
+                if (tmp - resultsFromEnd[i] < tmp - resultsFromBeginning[i])
+                {
+                    numberOfMoreAccurateResults++;
+                }
+            }
+
+            return numberOfMoreAccurateResults;
+
+        }*/
+
+    }
+}

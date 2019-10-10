b99f7a071e3b9850e4eb82f24f2d71ba1ee80172
mwirzba <mateuszwir23@gmail.com>
2019-10-10 03:51:58 +0200

    Adding Chart

--- SummationOfPowerSeries/SummationOfPowerSeries/Program.cs
+++ SummationOfPowerSeries/SummationOfPowerSeries/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
+using PdfSharp.Charting;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
 
 namespace SummationOfPowerSeries
 {
@@ -30,35 +34,17 @@ namespace SummationOfPowerSeries
             }
 
 
-            Console.WriteLine(Program.Compare(formulaResultsFromBegin, formulaResultsFromEnd,argumentsList)/numberOfArgs);
-            Console.WriteLine(Program.Compare(countUsingPreviousWordResultsFromBegin, countUsingPreviousWordResultsFromEnd, argumentsList)/numberOfArgs);
+            double cmpResult1 = Program.Compare(formulaResultsFromBegin, formulaResultsFromEnd,argumentsList)/numberOfArgs;
+            double cmpResult2 = Program.Compare(countUsingPreviousWordResultsFromBegin,
+                                    countUsingPreviousWordResultsFromEnd, argumentsList) / numberOfArgs;
 
 
             Console.WriteLine();
 
-            Console.WriteLine(Program.Compare(formulaResultsFromBegin, countUsingPreviousWordResultsFromBegin, argumentsList) / numberOfArgs);
-            Console.WriteLine(Program.Compare(formulaResultsFromEnd, countUsingPreviousWordResultsFromEnd, argumentsList) / numberOfArgs);
+            double cmpResult3 = Program.Compare(formulaResultsFromBegin, countUsingPreviousWordResultsFromBegin, argumentsList) / numberOfArgs;
+            double cmpResult4 = Program.Compare(formulaResultsFromEnd, countUsingPreviousWordResultsFromEnd, argumentsList) / numberOfArgs;
 
-       
-
-            /* alg.CountUsingFormula(1.55,8);
-             double rtn = alg.SumFromBeginning(resultsList: alg.ResultsList);
-             double rtn2 = alg.SumFromEnd(resultsList: alg.ResultsList);
-             alg.CountUsingPreviousResult(1.55,8);
-
-
-             double rtn1 = alg.SumFromBeginning(resultsList: alg.ResultsList2);
-             double rtn12 = alg.SumFromEnd(resultsList: alg.ResultsList2);
-
-             Console.WriteLine("1.1: "+rtn);
-             Console.WriteLine("2.1: "+ rtn2);
-             Console.WriteLine("Log: "+ Math.Log(1.55));
-
-
-             Console.WriteLine("1.2: " + rtn1);
-             Console.WriteLine("2.2: " + rtn12);
-             Console.WriteLine("Log: " +Math.Log(1.55));
-             */
+            AddColumnChartToPDF( cmpResult1,  cmpResult2,  cmpResult3,  cmpResult4);
 
             Console.ReadKey();
 
@@ -80,21 +66,80 @@ namespace SummationOfPowerSeries
 
         }
 
-        /*static int CheckIfAlgorithmUsingPreviousWordIsMoreAccurate(List<double> resultsFromBeginning, List<double> resultsFromEnd, List<double> args)
+        // ReSharper disable once InconsistentNaming
+        private static void AddColumnChartToPDF(double cmpResult1, double cmpResult2, double cmpResult3, double cmpResult4)
         {
-            int numberOfMoreAccurateResults = 0;
-            for (int i = 0; i < resultsFromBeginning.Count; i++)
-            {
-                double tmp = Math.Log(args[i]);
-                if (tmp - resultsFromEnd[i] < tmp - resultsFromBeginning[i])
-                {
-                    numberOfMoreAccurateResults++;
-                }
-            }
+            // Create a new PDF document.
+            PdfDocument document = new PdfDocument();
 
-            return numberOfMoreAccurateResults;
+            // Create a page.
+            PdfPage page = document.AddPage();
 
-        }*/
+            // Generate a 2d column chart graph
+            Chart chart = ColumnChart(cmpResult1, cmpResult2, cmpResult3, cmpResult4);
+
+            // Create a chart frame, set chart location and size
+            ChartFrame chartFrame = new ChartFrame();
+            chartFrame.Location = new XPoint(30, 30);
+            chartFrame.Size = new XSize(500, 600);
+            chartFrame.Add(chart);
+
+            // Render chart symbols into pdf page
+            XGraphics g = XGraphics.FromPdfPage(page);
+            chartFrame.Draw(g);
+
+            // Save and show the document           
+            document.Save("ColumnChart.pdf");
+            Process.Start("ColumnChart.pdf");
+        }
+
+        static Chart ColumnChart(double cmpResult1, double cmpResult2, double cmpResult3, double cmpResult4)
+        {
+            // Set chart type to Column2D
+            Chart chart = new Chart(ChartType.Column2D);
+
+            // Add first series of column chart with name and data
+            Series series = chart.SeriesCollection.AddSeries();
+            series.Name = "Series 1";
+            series.Add(new double[] { cmpResult1 });
+
+            // Add second series of column chart with name and data
+            series = chart.SeriesCollection.AddSeries();
+            series.Name = "Series 2";
+            series.Add(new double[] { cmpResult2 });
+
+            // Add third series of column chart with name and data
+            series = chart.SeriesCollection.AddSeries();
+            series.Name = "Series 3";
+            series.Add(new double[] { cmpResult3 });
+
+            // Add fourth series of column chart with name and data
+            series = chart.SeriesCollection.AddSeries();
+            series.Name = "Series 4";
+            series.Add(new double[] { cmpResult4 });
+
+            // Set X axes
+            chart.XAxis.TickLabels.Format = "00";
+            chart.XAxis.MajorTickMark = TickMarkType.Outside;
+            chart.XAxis.Title.Caption = "";
+
+            // Set Y axes
+            chart.YAxis.MajorTickMark = TickMarkType.Outside;
+            chart.YAxis.HasMajorGridlines = true;
+
+            // Set plot area (chart diagram)
+            chart.PlotArea.LineFormat.Color = XColors.DarkGray;
+            chart.PlotArea.LineFormat.Width = 1;
+            chart.PlotArea.LineFormat.Visible = true;
+
+            // Set legend
+            chart.Legend.Docking = DockingType.Right;
+
+            chart.DataLabel.Type = DataLabelType.Value;
+            chart.DataLabel.Position = DataLabelPosition.OutsideEnd;
+
+            return chart;
+        }
 
     }
 }
